ef4894365cfbf12b3b5d388751f772c82ea56cdf
kacperormaniec <kacperormaniec@gmail.com>
2025-01-04 13:22:14 +0100

    null-check

--- studentManagementSystem/Database/Extensions.cs
+++ studentManagementSystem/Database/Extensions.cs
@@ -12,6 +12,11 @@ public static class Extensions
         services.AddSingleton<DatabaseService>(provider =>
         {
             var connectionString = configuration["database:connectionStrings:postgres"];
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Connection string for PostgreSQL is null or empty.");
+            }
+            
             return new DatabaseService(connectionString ?? throw new InvalidOperationException("Connection string not found."));
         });
 
